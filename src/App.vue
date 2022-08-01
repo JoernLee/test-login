90539d215d7126b549e91e408338502a5ff7bd40
Joern <924901972@qq.com>
2022-08-01 20:53:11 +0800

    feat: add captcha function

--- src/App.vue
+++ src/App.vue
@@ -18,10 +18,11 @@
         </div>
         <div class="layui-form-item">
           <label class="layui-form-label">验证码</label>
-          <div class="layui-input-block">
+          <div class="layui-input-inline">
             <input type="text" name="title" required lay-verify="required" placeholder="请输入验证码" autocomplete="off"
                    class="layui-input">
           </div>
+          <div class="layui-form-mid" v-html="svg"></div>
         </div>
         <button type="button" class="layui-btn">点击登录</button>
         <a href="" class="imooc-link">忘记密码</a>
@@ -32,6 +33,27 @@
 </template>
 
 <script>
+import axios from 'axios';
+
+export default {
+  name: 'app',
+  data() {
+    return {
+      svg: ''
+    }
+  },
+  mounted() {
+    axios.get('http://localhost:3000/getCaptcha').then(res => {
+      console.log(res);
+      if (res.status === 200) {
+        let obj = res.data;
+        if (obj.code === 200) {
+          this.svg = obj.data;
+        }
+      }
+    });
+  }
+}
 
 </script>
 
