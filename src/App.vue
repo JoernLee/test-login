95d9f9589bedbeffdd98f015b8a3f4b8ef3a212e
Joern <924901972@qq.com>
2022-08-01 20:14:31 +0800

    feat: layout ui build

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,32 @@
 <template>
   <div id="app">
-    123
+    <div class="layui-container">
+      <form class="layui-form layui-form-pane">
+        <div class="layui-form-item">
+          <label class="layui-form-label">用户名</label>
+          <div class="layui-input-block">
+            <input type="text" name="title" required lay-verify="required" placeholder="请输入标题" autocomplete="off"
+                   class="layui-input">
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">密码</label>
+          <div class="layui-input-block">
+            <input type="password" name="title" required lay-verify="required" placeholder="请输入密码" autocomplete="off"
+                   class="layui-input">
+          </div>
+        </div>
+        <div class="layui-form-item">
+          <label class="layui-form-label">验证码</label>
+          <div class="layui-input-block">
+            <input type="text" name="title" required lay-verify="required" placeholder="请输入验证码" autocomplete="off"
+                   class="layui-input">
+          </div>
+        </div>
+        <button type="button" class="layui-btn">点击登录</button>
+        <a href="" class="imooc-link">忘记密码</a>
+      </form>
+    </div>
   </div>
 
 </template>
@@ -9,13 +35,25 @@
 
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  background: #f2f2f2;
 }
+
+.layui-container {
+  background: #fff;
+}
+
+input {
+  width: 190px
+}
+
+.imooc-link {
+  margin-left: 10px;
+
+  &:hover {
+    color: #009688;
+  }
+}
+
 </style>
